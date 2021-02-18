6405ebfb5faf2b4a36c638101e755aee36e306bf
Bella Amandine <bellamandy45@gmail.com>
2021-02-18 23:04:51 +0200

    Setup database

--- hospital_project/settings.py
+++ hospital_project/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'hospital',
-        'USER': 'ornella',
-        'PASSWORD': 'kko',
+        'USER': 'amandine',
+        'PASSWORD': 'pass',
     }
 }
 
