4cf8a9df97868bfe097c57ffbd72470e0ae057a5
Zilfa Cyamani <zilcyam@gmail.com>
2021-02-19 15:54:15 +0200

    creating template files

--- hospital_project/urls.py
+++ hospital_project/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hospital/',include('hospital.urls')),
+    url(r'',include('hospital.urls')),
 ]
