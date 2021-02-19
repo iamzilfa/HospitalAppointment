78b2d9429a961f4674a59dd02ec34ff4324dbd53
Bella Amandine <bellamandy45@gmail.com>
2021-02-19 10:33:13 +0200

    Add new-department form and all-department in templates

--- hospital/urls.py
+++ hospital/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    url('^admin/',views.admin, name = 'main-dashboard')
+    # url('^admin/',views.admin, name = 'main-dashboard'),
+    url('^admin/departments',views.all_departments, name = 'departments'),
+    url('^admin/new-department',views.new_department, name = 'new_department')
 ]    
--- hospital/views.py
+++ hospital/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .forms import DepartmentForm
 
 def index(request):
     return render(request, 'index.html')
 
-def admin(request):
-    return render(request, 'admin/main.html')
+def new_department(request):
+    departform = DepartmentForm()
+    return render(request, 'admin/new-department.html', {'form': departform})
+
+def all_departments(request):
+    return render(request, 'admin/all-departments.html')
+
+# def admin(request):
+#     return render(request, 'admin/main.html')
+
+# def add_department(request):
+#     departform = DepartmentForm()
+#     return render(request, 'admin/new-department.html', {'form': departform})
+
