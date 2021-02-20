5dce053547e6ed169f2e620b4e0f82cdcdc0bf92
pascaline-irabaruta <pascyirabaruta456@gmail.com>
2021-02-20 20:27:32 +0200

    added methods to save each class in the model

--- hospital/models.py
+++ hospital/models.py
@@ -8,6 +8,9 @@ class Patient(models.Model):
     email = models.EmailField()
     phone_number = models.IntegerField()
 
+    def save_patient(self):
+        self.save()
+
 class Hospital(models.Model):
     name = models.CharField(max_length=20)
 
@@ -15,6 +18,9 @@ class Department(models.Model):
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=600)
     hospitals = models.ForeignKey(Hospital,on_delete=models.CASCADE)
+
+    def save_department(self):
+        self.save()
 
 class Doctor(models.Model):
     first_name = models.CharField(max_length=20)
@@ -25,11 +31,20 @@ class Doctor(models.Model):
     doctor_image = models.ImageField(upload_to='images/')
     departments = models.ForeignKey(Department,on_delete=models.CASCADE)
 
+    def save_doctor(self):
+        self.save()
+
 class Schedule(models.Model):
     app_date = models.DateField()
     app_hour = models.CharField(max_length=30)
+
+    def save_schedule(self):
+        self.save()
 
 class Appointment(models.Model):
     doctors = models.ForeignKey(Doctor,on_delete=models.CASCADE)
     patients = models.ForeignKey(Patient,on_delete=models.CASCADE)
     schedules = models.ForeignKey(Schedule,on_delete=models.CASCADE,default='1')
+
+    def save_appointment(self):
+        self.save()
